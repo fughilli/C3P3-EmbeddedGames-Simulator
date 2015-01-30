d5c4685b8d5a93e31c1ff81490bdee4f410d3247
fughilli <kbalke@ucla.edu>
2015-01-29 18:44:43 -0800

    Restructured file layout

---
+++ bitmaps/littleman_fixed.h
@@ -0,0 +1,32 @@
+
+#ifndef _BITMAP_LITTLEMAN_FIXED_H_
+
+#define _BITMAP_LITTLEMAN_FIXED_H_
+
+
+
+#ifdef __cplusplus
+extern "C" {
+
+#endif
+
+
+
+#include <stdint.h>
+
+
+
+extern const uint8_t littleman_fixed[];
+
+
+
+#ifdef __cplusplus
+
+}
+
+#endif
+
+
+
+#endif // _BITMAP_LITTLEMAN_FIXED_H_
+
