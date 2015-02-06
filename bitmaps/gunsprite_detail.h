76f583077f3520603a62b676d16163169fe4f1a5
fughilli <kbalke@ucla.edu>
2015-02-06 12:38:26 -0800

    Added more sprites

---
+++ bitmaps/gunsprite_detail.h
@@ -0,0 +1,32 @@
+
+#ifndef _BITMAP_GUNSPRITE_DETAIL_H_
+
+#define _BITMAP_GUNSPRITE_DETAIL_H_
+
+
+
+#ifdef __cplusplus
+extern "C" {
+
+#endif
+
+
+
+#include <stdint.h>
+
+
+
+extern const uint8_t gunsprite_detail[];
+
+
+
+#ifdef __cplusplus
+
+}
+
+#endif
+
+
+
+#endif // _BITMAP_GUNSPRITE_DETAIL_H_
+
---
+++ bitmaps/gunsprite_mask.h
@@ -0,0 +1,32 @@
+
+#ifndef _BITMAP_GUNSPRITE_MASK_H_
+
+#define _BITMAP_GUNSPRITE_MASK_H_
+
+
+
+#ifdef __cplusplus
+extern "C" {
+
+#endif
+
+
+
+#include <stdint.h>
+
+
+
+extern const uint8_t gunsprite_mask[];
+
+
+
+#ifdef __cplusplus
+
+}
+
+#endif
+
+
+
+#endif // _BITMAP_GUNSPRITE_MASK_H_
+
