4c1de2abcff9433d76f233b1973e58024ed90fd9
fughilli <kbalke@ucla.edu>
2015-02-10 02:56:43 -0800

    Added bitmap from live demo

---
+++ bitmaps/smiley.h
@@ -0,0 +1,32 @@
+
+#ifndef _BITMAP_SMILEY_H_
+
+#define _BITMAP_SMILEY_H_
+
+
+
+#ifdef __cplusplus
+extern "C" {
+
+#endif
+
+
+
+#include <stdint.h>
+
+
+
+extern const uint8_t smiley[];
+
+
+
+#ifdef __cplusplus
+
+}
+
+#endif
+
+
+
+#endif // _BITMAP_SMILEY_H_
+
