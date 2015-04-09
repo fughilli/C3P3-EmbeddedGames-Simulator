c63c6744f0cb5659a9500ab5b0b100aa07ec206b
fughilli <kbalke@ucla.edu>
2015-04-09 14:55:32 -0700

    Added necessary setup code and comments

--- game/game.cpp
+++ game/game.cpp
@@ -1,11 +1,37 @@
 #include "game.h"
 
+/* TODO:    Your code goes here
+ *
+ *          Declare any global variables here
+ */
+
 void Game_init()
 {
+    // Enable double-buffering
+    TVOut_EnableDoubleBuffer(true);
 
+    // Enable buffer clearing on double buffer swap
+    TVOut_ClearBufferOnSwap(true);
+
+    /* TODO:    Your code goes here
+     *
+     *          Any code that needs to run once
+     *          to set up your game goes here
+     */
 }
 
 void Game_loop()
 {
+    /* TODO:    Your code goes here
+     *
+     *          Any code that modifies the screen
+     *          which needs to run once per frame
+     *          goes here
+     */
 
+    // Swap the buffer (present the drawing to the screen)
+    TVOut_Swap();
+
+    // Wait for the frame to complete drawing to the screen
+    TVOut_WaitFrames(1);
 }
