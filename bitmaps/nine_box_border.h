6d7ecadc2a72a3f3cc26315adaf5cd1ef7743ae0
fughilli <kbalke@ucla.edu>
2015-04-14 19:55:50 -0700

    Added sprite for use with boxsprite lib

---
+++ bitmaps/nine_box_border.h
@@ -0,0 +1,32 @@
+
+#ifndef _BITMAP_NINE_BOX_BORDER_H_
+
+#define _BITMAP_NINE_BOX_BORDER_H_
+
+
+
+#ifdef __cplusplus
+extern "C" {
+
+#endif
+
+
+
+#include <stdint.h>
+
+
+
+extern const uint8_t nine_box_border[];
+
+
+
+#ifdef __cplusplus
+
+}
+
+#endif
+
+
+
+#endif // _BITMAP_NINE_BOX_BORDER_H_
+
