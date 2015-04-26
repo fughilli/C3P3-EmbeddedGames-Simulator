bc031b2201fdc29d9bf40215bf0fd5a7c7cd291f
fughilli <kbalke@ucla.edu>
2015-04-26 06:09:37 -0700

    Changed display scaling to 2 by default; added support for button press detection

--- main.cpp
+++ main.cpp
@@ -10,13 +10,13 @@
 
 SDL_DisplayInterface* compat_disp_interface;
 
-Remote_buttons_t sim_hooked_buttons;
+Remote_buttons_t sim_hooked_buttons, sim_hooked_buttons_last;
 
 int main(int argc, char* argv[])
 {
     TVOut_Init();
     SDL_Window* window;
-    compat_disp_interface = SDL_DisplayInterface::createWindow(FB_WIDTH, FB_HEIGHT, window, 4);
+    compat_disp_interface = SDL_DisplayInterface::createWindow(FB_WIDTH, FB_HEIGHT, window, 2);
 
     SDL_SetHintWithPriority(SDL_HINT_RENDER_VSYNC, "1", SDL_HINT_OVERRIDE);
 
@@ -30,6 +30,8 @@ int main(int argc, char* argv[])
     {
         SDL_PumpEvents();
 
+        sim_hooked_buttons.raw &= ~0xFF00;
+
         if(SDL_PollEvent(&gameEvent))
         {
             if(gameEvent.type == SDL_QUIT || (gameEvent.type == SDL_KEYDOWN && gameEvent.key.keysym.scancode == SDL_SCANCODE_ESCAPE))
@@ -40,25 +42,26 @@ int main(int argc, char* argv[])
                 uint8_t buttonflag = 0;
                 if(gameEvent.type == SDL_KEYDOWN)
                     buttonflag = 1;
+
                 switch(gameEvent.key.keysym.scancode)
                 {
                 case SDL_SCANCODE_W:
-                    sim_hooked_buttons.buttons.up = buttonflag;
+                    sim_hooked_buttons.buttons.p_up = sim_hooked_buttons.buttons.up = buttonflag;
                     break;
                 case SDL_SCANCODE_A:
-                    sim_hooked_buttons.buttons.left = buttonflag;
+                    sim_hooked_buttons.buttons.p_left = sim_hooked_buttons.buttons.left = buttonflag;
                     break;
                 case SDL_SCANCODE_S:
-                    sim_hooked_buttons.buttons.down = buttonflag;
+                    sim_hooked_buttons.buttons.p_down = sim_hooked_buttons.buttons.down = buttonflag;
                     break;
                 case SDL_SCANCODE_D:
-                    sim_hooked_buttons.buttons.right = buttonflag;
+                    sim_hooked_buttons.buttons.p_right = sim_hooked_buttons.buttons.right = buttonflag;
                     break;
                 case SDL_SCANCODE_H:
-                    sim_hooked_buttons.buttons.a = buttonflag;
+                    sim_hooked_buttons.buttons.p_a = sim_hooked_buttons.buttons.a = buttonflag;
                     break;
                 case SDL_SCANCODE_U:
-                    sim_hooked_buttons.buttons.b = buttonflag;
+                    sim_hooked_buttons.buttons.p_b = sim_hooked_buttons.buttons.b = buttonflag;
                     break;
                 default:
                     break;
